10afdefe9ca9ab63e1ffff0d1502ca6695b1e4c1
Leo <xiaohul@utas.edu.au>
2022-08-19 05:36:45 +0000

    debug compile error for final tata

--- models/marts/intermediate/user_feature_2.sql
+++ models/marts/intermediate/user_feature_2.sql
@@ -7,7 +7,7 @@ uf2 as (
     Count(*) AS user_total_products,
     Count(DISTINCT product_id) AS user_distinct_products ,
     Sum(CASE WHEN reordered = 1 THEN 1 ELSE 0 END) / Cast(Sum(CASE WHEN order_number > 1 THEN 1 ELSE 0 END) AS DOUBLE)AS user_reorder_ratio
-FROM order_products_prior GROUP BY user_id
+FROM opp GROUP BY user_id
 )
 
 select * from uf2
--- models/staging/stg_order_products_prior.sql
+++ models/staging/stg_order_products_prior.sql
@@ -1,4 +1,4 @@
--- {{ config(materialized='table') }}
+
 
 with orders as (
  
