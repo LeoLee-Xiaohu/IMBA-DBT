91af7b7f1f2dacd8875a01cc52af89fd6a882468
Leo <xiaohul@utas.edu.au>
2022-08-19 07:05:58 +0000

    create data test method

--- models/marts/core/final_data.sql
+++ models/marts/core/final_data.sql
@@ -1,27 +1,43 @@
 
-
 with 
-pf as (
+PF as (
 select * from {{ ref('prd_features' )}}
 ),
 
-upf as (
+UF as (
     select * from {{ ref('up_features') }}
 ),
 
-uf1 as (
+F1 as (
     select * from {{ ref('user_feature_1') }}
 ),
 
-uf2 as (
+F2 as (
     select * from {{ ref('user_feature_2') }}
 ),
 
 final as (
-    select * from upf 
-    left join upf on pf.product_id = upf.product_id 
-    left join uf1 on upf.user_id = uf1.user_id 
-    left join uf2 on uf1.user_id = uf2.user_id 
+    select F1.user_id, 
+        F1.user_orders, 
+        F1.user_period, 
+        F1.user_mean_days_since_prior,
+        F2.user_total_products, 
+        F2.user_distinct_products, 
+        F2.user_reorder_ratio,
+        UF.product_id, 
+        UF.up_orders, 
+        UF.up_first_order, 
+        UF.up_last_order, 
+        UF.up_average_cart_position,
+        PF.prod_orders, 
+        PF.prod_reorders, 
+        PF.prod_first_orders, 
+        PF.prod_second_orders
+    
+    from UF 
+    left join PF on PF.product_id = UF.product_id 
+    left join F1 on UF.user_id = F1.user_id 
+    left join F2 on F1.user_id = F2.user_id 
 )
 
 select * from final 
